1c9f7a599a03f751688837101a554e8f940afb4e
Ivan Tham <pickfire@riseup.net>
2018-09-10 17:01:41 +0800

    Update url and drop python 2 support

--- ate.py
+++ ate.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
-try:  # python 3
-    from urllib.request import urlopen
-except ImportError:
-    from urllib import urlopen
-import contextlib
+import urllib.request
 import datetime
+import gzip
 import json
-import io
 
 
 def parse_dt(item, end=False):
     d = datetime.datetime.strptime(item['DATESTAMP'], '%d-%b-%y').date()
     s = ['TIME_FROM', 'TIME_TO'][end]
-    t = datetime.datetime.strptime(item[s], '%H:%M %p').time()
+    t = datetime.datetime.strptime(item[s], '%I:%M %p').time()
     return datetime.datetime.combine(d, t).strftime('%Y%m%dT%H%M%S')
 
 
@@ -21,20 +17,20 @@ def cli():
     parser = argparse.ArgumentParser(description='Export timetable into .ical')
     parser.add_argument(
         '--url',
-        default='https://ws.apiit.edu.my/web-services/index.php/open/weektimetable',
+        default='http://s3-ap-southeast-1.amazonaws.com/open-ws/weektimetable',
         help='url of apu open api')
-    parser.add_argument('-i', required=True, help='intake code', dest='intake')
-    parser.add_argument('-o', default='/dev/stdout',
-                        help='output', metavar='/dev/stdout', dest='output')
+    parser.add_argument('-i', dest='intake', help='intake code', required=True)
+    parser.add_argument('-o', dest='output', default='/dev/stdout',
+                        help='output destination', metavar='/dev/stdout')
     return parser.parse_args()
 
 
 def main(parser=None):
-    with contextlib.closing(urlopen(parser.url)) as url:
+    with gzip.open(urllib.request.urlopen(parser.url)) as url:
         data = json.loads(url.read())
     now = datetime.datetime.now().strftime('%Y%m%dT%H%M%S')
 
-    with io.open(parser.output, 'w', newline='\r\n') as f:
+    with open(parser.output, 'w', newline='\r\n') as f:
         f.write(u'BEGIN:VCALENDAR\n')
         f.write(u'VERSION:2.0\n')
         f.write(u'PRODID:-//%s timetable////\n' % parser.intake)
@@ -49,7 +45,7 @@ def main(parser=None):
             f.write(u'DTEND:%s\n' % parse_dt(item, end=True))
             f.write(u'END:VEVENT\n')
 
-        f.write(u'END:VCALENDAR')
+        f.write(u'END:VCALENDAR\n')
 
 
 if __name__ == '__main__':
